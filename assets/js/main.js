6d480db8ea5f5b7203aa683b2b2fd301ef4f4fec
Juan Macagno <jota@Juans-Mac-mini.local>
2023-08-25 11:00:23 -0300

    animaciones de ejemplo

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,3 +4,23 @@ $('.box h3').click(function(){
     $(this).next().toggleClass('active');
 })
 });
+
+
+
+function show() {
+    var reveal = document.querySelectorAll(".reveal");
+  
+    for (var i = 1; i < reveal.length; i++) {
+      var windowHeight = window.innerHeight;
+      var elementTop = reveal[i].getBoundingClientRect().top;
+      var e = 500;
+  
+      if (elementTop < windowHeight-e) {
+        reveal[i].classList.add("active");}
+      else {
+        reveal[i].classList.remove("active");
+      }
+    }
+  }
+  
+  window.addEventListener("scroll", show);
